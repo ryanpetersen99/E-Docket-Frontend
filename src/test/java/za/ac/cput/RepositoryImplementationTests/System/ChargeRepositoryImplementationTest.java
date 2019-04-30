83f863ec347c3ce5568c157df3fa35b128be37e5
ryanpetersen99 <47520518+ryanpetersen99@users.noreply.github.com>
2019-05-01 00:55:43 +0200

    Updated Tests Again

--- src/test/java/za/ac/cput/RepositoryImplementationTests/System/ChargeRepositoryImplementationTest.java
+++ src/test/java/za/ac/cput/RepositoryImplementationTests/System/ChargeRepositoryImplementationTest.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import za.ac.cput.Domain.System.Charge;
+import za.ac.cput.Factory.System.ChargeFactory;
 import za.ac.cput.Repository.System.ChargeRepository;
 import za.ac.cput.Repository.Implementation.System.ChargeRepositoryImplementation;
 
@@ -12,35 +13,54 @@ import java.util.Set;
 public class ChargeRepositoryImplementationTest {
 
     private ChargeRepository chargeRepository;
+    private Charge charge;
+
+    public Charge getSavedCharges() {
+        Set<Charge> chargeSet = this.chargeRepository.getChargeSet();
+        return chargeSet.iterator().next();
+    }
 
     @Before
     public void setUp() throws Exception{
         this.chargeRepository = ChargeRepositoryImplementation.getRepository();
+        this.charge = ChargeFactory.getCharge("Murder",1);
     }
 
     @Test
     public void create(){
-        this.chargeRepository.create(null);
-        Assert.assertEquals(null,null);
+        Charge createdI = this.chargeRepository.create(this.charge);
+        System.out.println("Successfully created charge" + "\n" + charge);
+        Assert.assertSame(createdI,this.charge);
     }
 
     @Test
     public void update(){
-
+        int no = 2;
+        Charge charge =  new Charge.Builder().copy(getSavedCharges()).noOfChargers(no).build();
+        System.out.println("Updating" + "\n" + charge );
+        Charge updatedNO = this.chargeRepository.update(charge);
+        System.out.println("Updated" + "\n" + updatedNO);
+        Assert.assertSame(no,updatedNO.getNoOfCharges());
     }
 
     @Test
     public void delete(){
+        Charge chargeSaved = getSavedCharges();
+        this.chargeRepository.delete(getSavedCharges().getNatureOfCharge());
     }
 
     @Test
     public void read(){
-
+        Charge chargeSaved = getSavedCharges();
+        System.out.println("Read charge" + "\n" + chargeSaved.getNatureOfCharge());
+        Charge read = this.chargeRepository.read(chargeSaved.getNatureOfCharge());
+        System.out.println("Read" + "\n" + read);
+        Assert.assertEquals(chargeSaved,read);
     }
 
     @Test
-    public void getChargeSet(){
+    public void getInspectorSet(){
         Set<Charge> chargeSet = this.chargeRepository.getChargeSet();
-        Assert.assertEquals(1,chargeSet.size());
+        System.out.println("List of Charges" + "\n" + chargeSet);
     }
 }
--- src/test/java/za/ac/cput/RepositoryImplementationTests/System/PoliceStationRepositoryImplementationTest.java
+++ src/test/java/za/ac/cput/RepositoryImplementationTests/System/PoliceStationRepositoryImplementationTest.java
@@ -3,6 +3,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import za.ac.cput.Domain.System.PoliceStation;
+import za.ac.cput.Factory.System.PoliceStationFactory;
+import za.ac.cput.Repository.Implementation.System.PendingCaseRepositoryImplementation;
 import za.ac.cput.Repository.Implementation.System.PoliceStationRepositoryImplementation;
 import za.ac.cput.Repository.System.PoliceStationRepository;
 
@@ -12,35 +14,53 @@ import java.util.Set;
 public class PoliceStationRepositoryImplementationTest {
 
     private PoliceStationRepository policeStationRepository;
+    private PoliceStation policeStation;
 
-    @Before
-    public void setUp() throws Exception{
-        this.policeStationRepository = PoliceStationRepositoryImplementation.getRepository();
+    public PoliceStation getSavedStation() {
+        Set<PoliceStation> policeStationSet = this.policeStationRepository.getPoliceStationSet();
+        return policeStationSet.iterator().next();
     }
 
+    @Before
+    public void setUp() throws Exception {
+        this.policeStationRepository = PoliceStationRepositoryImplementation.getRepository();
+        this.policeStation = PoliceStationFactory.getPoliceStation("Mitchells Plain Station");
+    }
     @Test
     public void create(){
-        this.policeStationRepository.create(null);
-        Assert.assertEquals(null,null);
+        PoliceStation createdI = this.policeStationRepository.create(this.policeStation);
+        System.out.println("Successfully created Police Station" + "\n" + policeStationRepository);
+        Assert.assertSame(createdI,this.policeStation);
     }
 
     @Test
     public void update(){
-
+        String name = "Woodstock Station";
+        PoliceStation policeStation =  new PoliceStation.Builder().copy(getSavedStation()).stationName(name).build();
+        System.out.println("Updating" + "\n" + policeStation );
+        PoliceStation updatedName = this.policeStationRepository.update(policeStation);
+        System.out.println("Updated" + "\n" + updatedName);
+        Assert.assertSame(name,updatedName.getStationName());
     }
 
     @Test
     public void delete(){
+        PoliceStation policeStation = getSavedStation();
+        this.policeStationRepository.delete(getSavedStation().getStationName());
     }
 
     @Test
     public void read(){
-
+        PoliceStation policeStation = getSavedStation();
+        System.out.println("Read station name" + "\n" + getSavedStation().getStationName());
+        PoliceStation read = this.policeStationRepository.read(getSavedStation().getStationName());
+        System.out.println("Read" + "\n" + read);
+        Assert.assertEquals(getSavedStation(),read);
     }
 
     @Test
-    public void getPoliceStationSet(){
+    public void getEvidenceSet(){
         Set<PoliceStation> policeStationSet = this.policeStationRepository.getPoliceStationSet();
-        Assert.assertEquals(0,policeStationSet.size());
+        System.out.println("List of Police Stations" + "\n" + policeStationSet);
     }
 }
